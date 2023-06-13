c5730e90da9ab0428800d7ae0284d8f398c36465
Elliot Thomas <eth0net@pm.me>
2023-06-13 14:05:33 +0100

    add cow movement

--- animals/cow.gd
+++ animals/cow.gd
@@ -2,35 +2,83 @@ extends CharacterBody2D
 
 
 enum State {
-	ACTION,
 	IDLE,
 	MOVE,
 }
 
 
-@export var acceleration = 800
-@export var friction = 400
-@export var speed = 100
+@export var acceleration = 100
+@export var friction = 100
+@export var speed = 10
 
-var state = State.MOVE
+@export var min_idle_time = 2
+@export var max_idle_time = 3
+@export var min_move_time = 1
+@export var max_move_time = 2
+
+var state = State.IDLE
 var input_vector = Vector2.ZERO
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 
 func _ready():
-	start_idle_timer()
+	get_input_vector()
+	start_timer()
 
 
 func _physics_process(delta):
-	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	update_velocity(delta)
+	update_animations()
 	move_and_slide()
 
 
 func _on_timer_timeout():
-	animated_sprite.play("idle")
-	start_idle_timer()
+	select_state()
+	get_input_vector()
+	start_timer()
 
 
-func start_idle_timer():
-	$Timer.start(randi_range(1, 3))
+func start_timer():
+	var timeout
+	match state:
+		State.IDLE:
+			timeout = randf_range(min_idle_time, max_idle_time)
+		State.MOVE:
+			timeout = randf_range(min_move_time, max_move_time)
+	$Timer.start(timeout)
+
+
+func select_state():
+	if state == State.MOVE:
+		state = State.IDLE
+	else:
+		state = randi_range(State.IDLE, State.MOVE)
+
+
+func get_input_vector():
+	input_vector = Vector2(
+		randi_range(-1, 1),
+		randi_range(-1, 1),
+	)
+
+
+func update_animations():
+	if velocity == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("move")
+	
+	if input_vector.x != 0:
+		animated_sprite.flip_h = input_vector.x < 0
+
+
+func update_velocity(delta):
+	match state:
+		State.IDLE:
+			velocity = Vector2.ZERO
+		State.MOVE:
+			if input_vector != Vector2.ZERO:
+				velocity = velocity.move_toward(input_vector * speed, acceleration * delta)
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, friction * delta)

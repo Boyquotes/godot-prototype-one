a8085afdcfe63fdc4a8c5617d2649604b164da1c
Elliot Thomas <eth0net@pm.me>
2023-06-15 12:39:22 +0100

    fix unused vars in chest

--- objects/chest.gd
+++ objects/chest.gd
@@ -4,9 +4,9 @@ extends StaticBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 
-func _on_interaction_hitbox_area_entered(area):
+func _on_interaction_hitbox_area_entered(_area):
 	animated_sprite.play("open")
 
 
-func _on_interaction_hitbox_area_exited(area):
+func _on_interaction_hitbox_area_exited(_area):
 	animated_sprite.play_backwards("open")
